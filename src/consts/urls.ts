afe0aef594674fecf467e12b001d81d9e2154ae6
overthestream <njhnjh02@naver.com>
2022-11-30 21:41:41 +0900

    fix: change provider to korean

--- src/consts/urls.ts
+++ src/consts/urls.ts
@@ -2,36 +2,36 @@ import { page } from '@/interfaces/urls';
 
 const urls: page[] = [
   {
-    provider: 'info',
+    provider: '정보대학',
     categoryId: 1,
     url: 'https://info.korea.ac.kr/info/board/notice_under.do',
   },
   {
-    provider: 'info',
+    provider: '정보대학',
     categoryId: 2,
     url: 'https://info.korea.ac.kr/info/board/news.do',
   },
   {
-    provider: 'info',
+    provider: '정보대학',
     categoryId: 3,
     url: 'https://info.korea.ac.kr/info/board/course.do',
   },
   {
-    provider: 'and',
+    provider: '디자인조형학부',
     categoryId: 4,
     url: 'https://and.korea.ac.kr/kuand/reference/notice.do',
   }, {
-    provider: 'and',
+    provider: '디자인조형학부',
     categoryId: 5,
     url: 'https://and.korea.ac.kr/kuand/reference/employment.do',
   },
   {
-    provider: 'and',
+    provider: '디자인조형학부',
     categoryId: 6,
     url: 'https://and.korea.ac.kr/kuand/reference/contest.do',
   },
   {
-    provider: 'chem',
+    provider: '화학과',
     categoryId: 7,
     url: 'http://chem.korea.ac.kr/news01',
   },
