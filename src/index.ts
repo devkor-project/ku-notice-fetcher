595d1e4eecac277f20a28547903cb4af9d013a0d
overthestream <njhnjh02@naver.com>
2023-03-14 15:18:07 +0900

    feat : add health check

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fetcherApp from '@/app/index';
 import schedule from 'node-schedule';
+import express from 'express';
 
 const app = async () => {
   // TODO:: logger
@@ -11,7 +12,13 @@ const app = async () => {
   }
 };
 
-const rule = new schedule.RecurrenceRule();
-rule.hour = 8;
-rule.minute = 0;
-schedule.scheduleJob(rule, app);
+const expressApp = express();
+expressApp.get('/', (req, res) => {
+  res.send('crawler is avaliable');
+});
+expressApp.listen(3000, () => {
+  const rule = new schedule.RecurrenceRule();
+  rule.hour = 8;
+  rule.minute = 0;
+  schedule.scheduleJob(rule, app);
+});
